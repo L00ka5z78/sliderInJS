da84132a00820e6a27ab803527f034208f3b8e8a
Lukasz Bolach <bravo1_lima1@yahoo.com>
2022-11-08 21:30:02 +0100

    slider feature created

--- slider.js
+++ slider.js
@@ -1,1 +1,25 @@
-console.log('works well')
+
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+const slider = document.querySelector(".slider");
+const images = document.querySelectorAll(".image");
+
+let slideNumber = 1;
+const length = images.length;
+
+const nextSlide = () => {
+    slider.style.transform = `translateX(-${slideNumber * 600}px)`;
+    slideNumber++;
+}
+
+const previousSlide = () => {
+    slider.style.transform = `translateX(0px)`;
+    slideNumber = 1;
+}
+
+right.addEventListener("click", () => {
+    slideNumber < images.length ? nextSlide() : previousSlide();
+
+});
+
+
